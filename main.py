db69239c328b237ad58152593ec68efb583472bb
den1slx <121083421+den1slx@users.noreply.github.com>
2023-02-19 17:47:05 +1000

    Initial commit

---
+++ main.py
@@ -0,0 +1,88 @@
+import requests
+import os
+from urllib.parse import urlparse
+import argparse
+from dotenv import load_dotenv
+
+
+def shorten_link(token, url):
+    bitly_url = 'https://api-ssl.bitly.com/v4/bitlinks'
+    headers = {'Authorization': f'Bearer {token}'}
+    body = {'long_url': url}
+    response = requests.post(bitly_url, json=body, headers=headers)
+    response.raise_for_status()
+    return response.json()['link']
+
+
+def count_clicks(bitlink, token):
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
+    headers = {'Authorization': f'Bearer {token}'}
+    params = {
+        'units': -1,
+    }
+    response = requests.get(url, params=params, headers=headers)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def remove_link_protocol(link):
+    parsed_link = urlparse(link)
+    redacted_link = ''
+    for link_component in parsed_link:
+        if link_component == parsed_link[0]:
+            continue
+        if parsed_link[0] == '' and parsed_link[1] == '' and 'bit.ly' in parsed_link[2]:
+            return link
+        redacted_link += link_component
+    return redacted_link
+
+
+def is_link(link):
+    try:
+        requests.get(link)
+    except requests.exceptions.MissingSchema:
+        return False
+    except requests.exceptions.InvalidSchema:
+        return False
+    except requests.exceptions.InvalidURL:
+        return False
+    else:
+        return True
+
+
+def is_bitlink(link, token):
+    headers = {'Authorization': f'Bearer {token}'}
+    link = remove_link_protocol(link)
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{link}', headers=headers)
+    return response.ok
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('link', help='your link')
+    return parser
+
+
+def main():
+    load_dotenv()
+    token = os.environ['BITLY_TOKEN']
+    parser = create_parser()
+    namespace = parser.parse_args()
+    url = namespace.link
+    try:
+        if is_link(url) or is_bitlink(url, token):
+            if is_bitlink(url, token):
+                parsed_bitlink = remove_link_protocol(url)
+                clicks = count_clicks(parsed_bitlink, token)
+                print(f'Количество кликов: {clicks}')
+            else:
+                bitlink = shorten_link(token, url)
+                print('bitlink:', bitlink)
+        else:
+            print('Не ссылка или некорректная ссылка')
+    except requests.exceptions.HTTPError:
+        print('Некорректная ссылка ')
+
+
+if __name__ == '__main__':
+    main()
